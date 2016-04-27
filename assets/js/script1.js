7bec9fd3f92a1c3f004c85766f6bf824043b2a37
e4journal <sierra.balatan@berkeley.edu>
2016-04-26 20:38:15 -0700

    Blog

---
+++ assets/js/script1.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  // Show/Hide Sidebar
+  $("#sidebar-button").click(function() {
+    if ($(".sidebar-container").hasClass("sidebar-active")) {
+      $("body").removeClass("no-scroll");
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+    }
+    else {
+      $("#sidebar-button").addClass("button-active");
+      $(".sidebar-container").addClass("sidebar-active");
+      $(".page-wrapper").addClass("wrapper-active");
+      setTimeout(function() {
+        $('body').addClass('no-scroll');
+      }, 300);
+    }
+  });
+
+  //Hide Sidebar
+  $(".page-wrapper").click(function(){
+    if ($(".sidebar-container").hasClass("sidebar-active")) {
+      $("body").removeClass("no-scroll");
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+    }
+  });
+
+
+  // "SHOW MODAL" WHEN "CLICK ON WRITE BUTTON FROM MAIN PAGE"
+  $(".write-circle").click(function(){
+      $("div#modal-container").show();
+  });
+  // "HIDE MODAL" WHEN "CLICK ON MODAL OVERLAY"
+  $("#modal-overlay").click(function(){
+      $("div#modal-container").hide();
+  });
+
+
+  // REDIRECT WHEN RIGHT PASSCODE IS ENTERED
+  $("#modal-footer").click(
+    function validate(){
+      var input = $(".input").val()
+      console.log(input)
+      var passwords=['UiC','Ephesians4:29'];
+      for(var i=0;i<passwords.length;i++) {
+          if(input==passwords[i]){
+              window.location.href = "index3.html";
+              return true;
+          }
+      }
+      alert("Incorrect Password. Try again.");
+      return false;
+    });
+
+  
+//
+});
